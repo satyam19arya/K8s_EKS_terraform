007d16ca8906f4a47325e863464c0a81c9917bde
Satyam <satyam19arya@gmail.com>
2023-08-19 18:49:43 +0530

    first commit

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -6,10 +6,6 @@ output "PROJECT_NAME" {
   value = var.PROJECT_NAME
 }
 
-output "VPC_ID" {
-  value = aws_vpc.vpc.id
-}
-
 output "PUB_SUB_1_A_ID" {
   value = aws_subnet.pub-sub-1-a.id
 }
@@ -24,8 +20,4 @@ output "PRI_SUB_3_A_ID" {
 
 output "PRI_SUB_4_B_ID" {
   value = aws_subnet.pri-sub-4-b.id
-}
-
-output "IGW_ID" {
-  value = aws_internet_gateway.internet_gateway.id
 }
