85c942a6f145d67ae1fe47ae2fc5bff34e2923ce
Satyam <satyam19arya@gmail.com>
2023-08-19 19:27:09 +0530

    first commit

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -4,10 +4,6 @@ resource "aws_vpc" "vpc" {
   instance_tenancy     = "default"
   enable_dns_hostnames = true
   enable_dns_support   = true
-  enable_classiclink   = false
-
-  enable_classiclink_dns_support = false
-  assign_generated_ipv6_cidr_block = false
 
   tags = {
     Name = "${var.PROJECT_NAME}-vpc"
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -5,7 +5,3 @@ variable "PUB_SUB_1_A_CIDR" {}
 variable "PUB_SUB_2_B_CIDR" {}
 variable "PRI_SUB_3_A_CIDR" {}
 variable "PRI_SUB_4_B_CIDR" {}
-variable "PUB_SUB_1_A_ID" {}
-variable "PUB_SUB_2_B_ID" {}
-variable "PRI_SUB_3_A_ID" {}
-variable "PRI_SUB_4_B_ID" {}
