cef64ece660700611baa5e2e2b6e92653522607d
Satyam <satyam19arya@gmail.com>
2023-09-17 20:56:56 +0530

    updated

--- modules/node_group/main.tf
+++ modules/node_group/main.tf
@@ -37,7 +37,7 @@ resource "aws_eks_node_group" "nodes_general" {
   force_update_version = false
 
   # List of instance types associated with the EKS Node Group
-  instance_types = ["t3.small"]
+  instance_types = ["t3.medium"]
 
   labels = {
     role = "${var.EKS_CLUSTER_NAME}-Node-group-role",
